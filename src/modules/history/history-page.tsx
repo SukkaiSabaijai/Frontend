6fb14a83f58db1b4b6f1158b4c653c2f743552bb
AgogFox <bopawat@gmail.com>
2024-10-22 20:41:40 +0700

    history page 50%

--- src/modules/history/history-page.tsx
+++ src/modules/history/history-page.tsx
@@ -1,28 +1,13 @@
 import Header from "@/shared/components/header/header";
 import Card from "@/shared/components/card/card"
-import axios from "axios"
-
-type Favorite = {
-  image: string,
-  nickname: string,
-  like: number,
-  dislike: number
-}
-
-type Response = {
-  error?: boolean,
-  status: boolean,
-  result: Favorite
-}
-
 
 const mock: string[] = ["Toilet1", "Toilet2", "Toilet3", "Toilet4", "Toilet5"];
 
-const BookmarkPage = () => {
+const HistoryPage = () => {
   return (
     <>
       <div className="bg-custom-light-blue">
-        <Header title="Bookmark" />
+        <Header title="History" />
         <div className="flex justify-center items-center mt-10 flex-col">
           {
             mock.map( (toilet_name) => {return (
@@ -37,4 +22,4 @@ const BookmarkPage = () => {
   );
 };
 
-export default BookmarkPage;
+export default HistoryPage;
