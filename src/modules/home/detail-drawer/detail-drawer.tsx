2fab91cff29f8a68395c52806a9b9ee1995c1242
ppookkiesbubu <pooky21147@gmail.com>
2024-10-26 21:33:34 +0700

    merge tup

--- src/modules/home/detail-drawer/detail-drawer.tsx
+++ src/modules/home/detail-drawer/detail-drawer.tsx
@@ -6,25 +6,34 @@ import Drawer from "@mui/material/Drawer";
 import Img from "@/shared/components/detail-drawer/detail_picture";
 import DetailCard from "@/shared/components/detail-drawer/detail_card";
 
-
 type Props = {
   openDrawer: UseBooleanReturn;
+  handleBackIconOnClick: () => void;
 };
 
 const images = [
-  { src: "https://f.ptcdn.info/892/034/000/1440753623-72-o.jpg", text: "12 Level Building" },
-  { src: "https://f.ptcdn.info/893/034/000/1440754319-73-o.jpg", text: "12 Level Building" },
-  { src: "https://www.horonumber.com/upload/uppic/1414671588.jpg", text: "12 Level Building" },
+  {
+    src: "https://f.ptcdn.info/892/034/000/1440753623-72-o.jpg",
+    text: "12 Level Building",
+  },
+  {
+    src: "https://f.ptcdn.info/893/034/000/1440754319-73-o.jpg",
+    text: "12 Level Building",
+  },
+  {
+    src: "https://www.horonumber.com/upload/uppic/1414671588.jpg",
+    text: "12 Level Building",
+  },
 ];
 
-const DetailDrawer = ({ openDrawer }: Props) => {
+const DetailDrawer = ({ openDrawer, handleBackIconOnClick }: Props) => {
   return (
     <Drawer
       open={openDrawer.value}
       anchor="bottom"
       sx={{
         "& .MuiDrawer-paper": {
-          height: "50%",
+          height: "60%",
           width: "100%",
           zIndex: "1500",
           position: "fixed",
@@ -34,14 +43,14 @@ const DetailDrawer = ({ openDrawer }: Props) => {
         },
       }}
     >
-      <Img images={images} className="w-full h-auto">
-
-      </Img>
-      <DetailCard description="ecc building 1 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520" latitude={37.7749} longitude={-122.4194} >
-
-      </DetailCard>
+      <Img images={images} className="w-full h-auto"></Img>
+      <DetailCard
+        description="ecc building 1 ถ. ฉลองกรุง แขวงลำปลาทิว เขตลาดกระบัง กรุงเทพมหานคร 10520"
+        latitude={37.7749}
+        longitude={-122.4194}
+      ></DetailCard>
       <ButtonIcon
-        onClick={openDrawer.onFalse}
+        onClick={handleBackIconOnClick}
         width={30}
         height={41}
         alt="rest-icon"
