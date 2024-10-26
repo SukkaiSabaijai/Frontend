1d5ca0d5a941ce62f4a5ac7f6d6799651ff0b3f4
ppookkiesbubu <pooky21147@gmail.com>
2024-10-26 21:38:30 +0700

    endpoint in project

--- src/modules/home/_services/home.service.ts
+++ src/modules/home/_services/home.service.ts
@@ -1,6 +1,5 @@
 import { endpoints } from "@/shared/configs/endpoints.config";
 import Axios from "@/shared/utils/axios";
-import axios from "axios";
 
 export const test = async () => {
   const { data } = await Axios.get("/");
--- src/shared/configs/endpoints.config.ts
+++ src/shared/configs/endpoints.config.ts
@@ -3,7 +3,30 @@
  */
 
 export const endpoints = {
-    toilet :{
-        create:'/toilet/create'
-    },
+  marker: {
+    create: `/markers/create`,
+    get: `/markers`,
+    getId: (id: string) => `/markers/${id}`,
+    delete: (id: string) => `/markers/${id}`,
+  },
+  user: {
+    register: `/auth/signup`,
+    login: `/auth/signin`,
+    logout: `/auth/logout`,
+    refresh: `/auth/refresh`,
+    editProfile: `/user`,
+    changePass: `/user/change-password`,
+    forgotPass: ``,
+  },
+  reviews: {
+    create: `/reviews/create`,
+    get: (markerId: string) => `/reviews/marker/${markerId}/reviews`,
+    delete: (markerId: string, Id: string) => `/reviews/delete/${markerId}/${Id}`,
+  },
+  bookmark: {
+    get: `/bookmarks`,
+    create: `/bookmarks`,
+    update: (id: string) => `/bookmarks/${id}`,
+    delete: (id: string) => `/bookmarks/${id}`,
+  },
 }
