3953ca59abb3efb26d9523e0fe64af8e8da8f735
boomoioi <n.nanthawisit@gmail.com>
2024-11-01 04:24:34 +0700

    add back button

--- src/modules/history/marker-history.tsx
+++ src/modules/history/marker-history.tsx
@@ -5,10 +5,12 @@ import { useEffect, useState } from "react";
 import { MarkerHistory } from "./_types/marker-history.type"
 import { deleteMarker, getMarkerHistory } from "./_services/history.service";
 import HistoryCard from "../../shared/components/card/history-card"
+import { useRouter } from "next/navigation";
+import ButtonIcon from "../../shared/components/button/button-icon";
 
 const MarkerHistoryPage = () => {
   const [markerHistories, setmarkerHistories] = useState<MarkerHistory[]>()
-  
+  const router = useRouter();
   const fetchMarkerHistories = async () => {
     const data = await getMarkerHistory();
     setmarkerHistories(data);
@@ -33,6 +35,16 @@ const MarkerHistoryPage = () => {
           )
         }
       </div>
+      <div className="absolute bottom-16 left-0 mb-4 ml-4">
+          <ButtonIcon
+            onClick={() => router.push("/")}
+            width={30}
+            height={41}
+            alt="back-icon"
+            src="/assets/icon/back.svg"
+            className="bg-custom-light-yellow"
+          />
+        </div>
     </div>
   );
 }
--- src/modules/history/review-history.tsx
+++ src/modules/history/review-history.tsx
@@ -5,9 +5,12 @@ import { useEffect, useState } from "react";
 import { ReviewHistory } from "./_types/review-history.type";
 import { deleteReview, getReviewHistory } from "./_services/history.service";
 import Header from "@/shared/components/header/header";
+import ButtonIcon from "../../shared/components/button/button-icon";
+import { useRouter } from "next/navigation";
 
 const ReviewHistoryPage = () => {
     const [reviewHistories, setReviewHistories] = useState<ReviewHistory[]>();
+    const router = useRouter();
     const fetchReivewHistories = async () => {
         const data = await getReviewHistory();
         setReviewHistories(data);
@@ -31,6 +34,16 @@ const ReviewHistoryPage = () => {
               )
             )
           }
+        </div>
+        <div className="absolute bottom-16 left-0 mb-4 ml-4">
+          <ButtonIcon
+            onClick={() => router.push("/")}
+            width={30}
+            height={41}
+            alt="back-icon"
+            src="/assets/icon/back.svg"
+            className="bg-custom-light-yellow"
+          />
         </div>
       </div>
     );
