16105ef3342b7a6df7013effe803b611278689e3
boomoioi <n.nanthawisit@gmail.com>
2024-11-01 01:34:55 +0700

    refactor: remove unused HistoryPage component and clean up related files; enhance header click functionality in Home, Login, and Register pages

--- src/modules/home/home-header.tsx
+++ src/modules/home/home-header.tsx
@@ -7,12 +7,19 @@ type Props = {
 
 const HomeHeader = ({ mode }: Props) => {
   const title =
-    mode == MarkerType.Toilet ? "Toilet Near Me 🚽" : "Seat Near Me 🪑";
+    mode === MarkerType.Toilet ? "Toilet Near Me 🚽" : "Seat Near Me 🪑";
   const style =
-    mode == MarkerType.Toilet ? "bg-custom-blue" : "bg-custom-yellow";
+    mode === MarkerType.Toilet ? "bg-custom-blue" : "bg-custom-yellow";
+
+  // Handler for clicking the header
+  const handleClick = () => {
+    window.location.href = "/"; // Redirect to home page using window.location
+  };
+
   return (
     <div
-      className={`${style} w-full h-28 rounded-b-2xl font-bold text-white font-mono text-3xl flex justify-center items-end pb-4 shadow-md shadow-slate-400 z-[1000] absolute`}
+      onClick={handleClick} // Add onClick handler
+      className={`${style} w-full h-28 rounded-b-2xl font-bold text-white font-mono text-3xl flex justify-center items-end pb-4 shadow-md shadow-slate-400 z-[1000] absolute cursor-pointer`} // Add cursor pointer style
     >
       {title}
     </div>
