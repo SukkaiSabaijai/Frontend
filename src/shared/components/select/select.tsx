26f924e2673f0613ce9aded6ffba49b35bb2ef48
boomoioi <n.nanthawisit@gmail.com>
2024-10-31 22:10:51 +0700

    refactor: enhance drawer and search components for better layout and responsiveness

--- src/shared/components/select/select.tsx
+++ src/shared/components/select/select.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import { MarkerType } from "@/modules/home/_types/home.type";
 import { useBoolean } from "@/shared/hooks/use-boolean";
-import React, { Dispatch, SetStateAction, useState } from "react";
+import React, { Dispatch, SetStateAction } from "react";
 
 type Props = {
   label: string;
@@ -14,10 +14,17 @@ type Props = {
 
 const Select = ({ label, setCategoryList, className, mode }: Props) => {
   const isSelect = useBoolean(false);
+  
+  // Define background color based on mode
   const bgColor =
-    mode == MarkerType.Toilet
+    mode === MarkerType.Toilet
+      ? isSelect.value
+        ? "bg-custom-light-yellow"
+        : "bg-custom-light-blue"
+      : isSelect.value
       ? "bg-custom-light-blue"
       : "bg-custom-light-yellow";
+
   const handleOnClick = () => {
     setCategoryList((prevList) => {
       if (isSelect.value) {
@@ -28,6 +35,7 @@ const Select = ({ label, setCategoryList, className, mode }: Props) => {
     });
     isSelect.onToggle();
   };
+
   return (
     <div
       className={cn(
@@ -37,27 +45,15 @@ const Select = ({ label, setCategoryList, className, mode }: Props) => {
     >
       <p>{label}</p>
       <div
-        className={`w-10 h-5 rounded-full flex items-center px-1 ${
-          mode == MarkerType.Toilet
-            ? isSelect.value
-              ? "bg-custom-light-yellow justify-end"
-              : "bg-custom-light-blue"
-            : isSelect.value
-            ? "bg-custom-light-blue justify-end"
-            : "bg-custom-light-yellow"
-        }`}
+        className={`w-10 h-5 rounded-full flex items-center px-1 cursor-pointer transition-colors duration-300 ${bgColor}`}
         onClick={handleOnClick}
+        aria-pressed={isSelect.value}
+        role="button"
       >
         <div
-          className={`w-4 h-4 rounded-full ${
-            mode == MarkerType.Toilet
-              ? isSelect.value
-                ? "bg-custom-yellow"
-                : "bg-custom-blue"
-              : isSelect.value
-              ? "bg-custom-blue"
-              : "bg-custom-yellow"
-          }`}
+          className={`w-4 h-4 rounded-full transition-transform duration-300 ${
+            isSelect.value ? "translate-x-5" : "translate-x-0"
+          } ${mode === MarkerType.Toilet ? "bg-custom-yellow" : "bg-custom-yellow"}`}
         ></div>
       </div>
     </div>
